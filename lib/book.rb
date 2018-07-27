e9c3e0e604b27eba71541dd3b04c19c657ad14fd
amulyamari <amulya@saimail.com>
2018-07-27 18:43:39 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
-# book.rb
-# Add you Book class here
+# class Book
+#   def initialize (title)
+#     @title = title
+  end
+  def title
+    @title
+  end
+  def author = (author)
+    @author = author 
+  end
+  def author
+    @author
+  end
+  def page_count
+    @page_count
+  end
+  def page_count = (page_count)
+    @page_count = page_count
+  end
+  
+  def genre
+    @genre
+  end
+  def genre = (genre)
+    @genre = genre
+  end
+  
+  def turn_page
+    puts "Wow you turned the page fast"
+  end 
+end
+
+
+book= Book.new("And then there were none")
